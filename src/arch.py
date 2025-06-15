f26d92bf6edae1f5d2d2903c0a1917b2b7af596d
Bivek Panthi <go34wun@mytum.de>
2025-06-15 11:42:53 +0200

    new updates

--- src/arch.py
+++ src/arch.py
@@ -23,34 +23,80 @@ def encoder():
 
 def NMR_head():
     combined_features = keras.layers.Input(shape=(None, 16), name="combined_features")
-    head_NMR_1 = keras.layers.Dense(64, activation="relu")(combined_features)
-    head_NMR_2 = keras.layers.Dense(32, activation="relu")(head_NMR_1)
-    output_head_NMR = keras.layers.Dense(1, activation="relu", name="g_scores")(head_NMR_2)
+    x = keras.layers.Dense(64, activation="relu")(combined_features)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Dense(32, activation="relu")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Dense(16, activation="relu")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Dense(8, activation="relu")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    output_head_NMR = keras.layers.Dense(1, activation="sigmoid", name="g_scores")(x)
     return keras.Model(inputs=combined_features, outputs=output_head_NMR)
 
 
 def DisProt_head():
     combined_features = keras.layers.Input(shape=(None, 16), name="combined_features")
-    head_DisProt_1 = keras.layers.Dense(64, activation="relu")(combined_features)
-    head_DisProt_2 = keras.layers.Dense(32, activation="relu")(head_DisProt_1)
-    pooled_output = keras.layers.GlobalAveragePooling1D()(head_DisProt_2)
+    x = keras.layers.Dense(64, activation="relu")(combined_features)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Dense(32, activation="relu")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Dense(16, activation="relu")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Dense(8, activation="relu")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    pooled_output = keras.layers.GlobalAveragePooling1D()(x)
     output_head_DisProt = keras.layers.Dense(1, activation="sigmoid", name="disorder_content")(pooled_output)
     return keras.Model(inputs=combined_features, outputs=output_head_DisProt)
 
 
 def SoftDis_head():
     combined_features = keras.layers.Input(shape=(None, 16), name="combined_features")
-    head_SoftDis_1 = keras.layers.Dense(64, activation="relu")(combined_features)
-    head_SoftDis_2 = keras.layers.Dense(32, activation="relu")(head_SoftDis_1)
-    output_head_SoftDis = keras.layers.Dense(1, activation="sigmoid", name="soft_disorder_frequency")(head_SoftDis_2)
+    x = keras.layers.Dense(64, activation="relu")(combined_features)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Dense(32, activation="relu")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Dense(16, activation="relu")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    x = keras.layers.Dense(8, activation="relu")(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.2)(x)
+
+    output_head_SoftDis = keras.layers.Dense(1, activation="sigmoid", name="soft_disorder_frequency")(x)
     return keras.Model(inputs=combined_features, outputs=output_head_SoftDis)
 
+
+def masked_mse_loss(y_true, y_pred, mask):
+    mask = tf.cast(mask, y_pred.dtype)
+    se = tf.square(y_true - y_pred) * mask
+    return tf.reduce_sum(se) / tf.reduce_sum(mask)
 
 
 class DeepEntropy(keras.Model):
     def __init__(self):
         super().__init__()
-
         self.NMR_head_loss_tracker = keras.metrics.Mean(name="NMR_head_loss")
         self.DisProt_head_loss_tracker = keras.metrics.Mean(name="DisProt_head_loss")
         self.SoftDis_head_loss_tracker = keras.metrics.Mean(name="SoftDis_head_loss")
@@ -66,11 +112,9 @@ class DeepEntropy(keras.Model):
         self.SoftDis_head_loss = []
         self.total_loss = []
 
-
     # @property
     # def metrics(self):
     #     return [self.NMR_head_loss_tracker, self.DisProt_head_loss_tracker, self.SoftDis_head_loss_tracker]
-
 
     def compile(self, NMR_optimizer, DisProt_optimizer, SoftDis_optimizer, NMR_head_loss_fn, DisProt_head_loss_fn, SoftDis_head_loss_fn):
         super().compile()
@@ -83,8 +127,54 @@ class DeepEntropy(keras.Model):
         self.SoftDis_head_loss_fn = SoftDis_head_loss_fn
 
 
+    def build_init(self, embedding_shape, plddt_shape):
+        input_embeddings = tf.zeros(embedding_shape, dtype=tf.float32)
+        input_plddt = tf.zeros(plddt_shape, dtype=tf.float32)
+
+        # NMR
+        with tf.GradientTape() as tape:
+            combined = self.encoder([input_embeddings, input_plddt])
+            predicted_g_scores = self.nmr_head_model(combined)
+            dummy_target_g = tf.zeros_like(input_plddt)
+            dummy_mask = tf.ones_like(dummy_target_g)
+            g_loss = self.NMR_head_loss_fn(dummy_target_g, predicted_g_scores, mask=dummy_mask)
+
+        g_vars = self.encoder.trainable_variables + self.nmr_head_model.trainable_variables
+        g_grads = tape.gradient(g_loss, g_vars)
+        g_grads = [tf.stop_gradient(g) if g is not None else None for g in g_grads]
+        self.NMR_optimizer.apply_gradients(zip(g_grads, g_vars))
+
+        # DisProt
+        with tf.GradientTape() as tape:
+            combined = self.encoder([input_embeddings, input_plddt])
+            predicted_disorder = self.DisProt_head_model(combined)
+            dummy_target_d = tf.zeros((1, 1), dtype=tf.float32)
+            d_loss = self.DisProt_head_loss_fn(dummy_target_d, predicted_disorder)
+
+        d_vars = self.encoder.trainable_variables + self.DisProt_head_model.trainable_variables
+        d_grads = tape.gradient(d_loss, d_vars)
+        d_grads = [tf.stop_gradient(g) if g is not None else None for g in d_grads]
+        self.DisProt_optimizer.apply_gradients(zip(d_grads, d_vars))
+
+        # SoftDis
+        with tf.GradientTape() as tape:
+            combined = self.encoder([input_embeddings, input_plddt])
+            predicted_soft_disorder = self.softdis_head_model(combined)
+            dummy_target_s = tf.zeros_like(input_plddt)
+            s_loss = self.SoftDis_head_loss_fn(dummy_target_s, predicted_soft_disorder)
+
+        s_vars = self.encoder.trainable_variables + self.softdis_head_model.trainable_variables
+        s_grads = tape.gradient(s_loss, s_vars)
+        s_grads = [tf.stop_gradient(g) if g is not None else None for g in s_grads]
+        self.SoftDis_optimizer.apply_gradients(zip(s_grads, s_vars))
+
+        print("All the heads and optimizers are initialized. Good luck with the training loop!")
+
+
+    @tf.function
     def train_step(self, data):
-        ((input_embeddings, input_plddt, target),) = data
+        ((input_embeddings, input_plddt, target, mask_tf),_,_) = data
+        # ((input_embeddings, input_plddt, target, mask_tf),) = data
         # (input_data, target) = data
         # (input_embeddings, input_plddt) = input_data
 
@@ -92,7 +182,7 @@ class DeepEntropy(keras.Model):
             with tf.GradientTape() as tape:
                 combined_features = self.encoder([input_embeddings, input_plddt])
                 predicted_g_scores = self.nmr_head_model(combined_features)
-                g_loss = self.NMR_head_loss_fn(target, predicted_g_scores)
+                g_loss = self.NMR_head_loss_fn(target, predicted_g_scores, mask=mask_tf)
 
             trainable_variables = self.encoder.trainable_variables + self.nmr_head_model.trainable_variables
             gradients = tape.gradient(g_loss, trainable_variables)
@@ -135,7 +225,17 @@ class DeepEntropy(keras.Model):
 
         return loss_metrics
 
+    def predict(self, data):
+        (input_embeddings, input_plddt) = data
+        combined_features = self.encoder([input_embeddings, input_plddt])
 
+        if self.target_flag_ == "g_scores":
+            return self.nmr_head_model(combined_features)
+        elif self.target_flag_ == "disprot_disorder":
+            return self.DisProt_head_model(combined_features)
+        else:
+            return self.softdis_head_model(combined_features)
+        
 
 if __name__ == '__main__':
     entropy = DeepEntropy()
